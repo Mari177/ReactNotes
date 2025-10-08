556bd8f8e7dbee572786b0af3954074053ee0862
Marisankar M <mailmarisankar@gmail.com>
2025-10-08 21:50:28 +0530

    Remove the comments in App.js

--- App.js
+++ App.js
@@ -1,18 +1,3 @@
-/**
- *
- * <div id="parent">
- *      <div id="child">
- *         <h1>I'm h1 tag</h1>
- *         <h2>I'm h2 tag</h2>
- *      </div>
- *      <div id="child2">
- *         <h1>I'm h1 tag</h1>
- *         <h2>I'm h2 tag</h2>
- *      </div>
- * </div>
- *
- * ReactElement(Object) => HTML(Browser Understands)
- */
 
 const parent = React.createElement("div", { id: "parent" }, [React.createElement("div", { id: "child1" }, [
     React.createElement("h1", {}, "I am h1 tag"), React.createElement("h2", {}, "I am h2 tag"),
