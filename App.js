c3031c3d2dc64eab975b3618f64d74a56310a2e8
Mari177 <mailmarisankar@gmail.com>
2025-11-01 10:15:41 +0530

    Chapter 4 -  Food app

--- App.js
+++ App.js
@@ -1,26 +1,86 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
+import { CDN_URL } from "./utils/constants";
+import { RESTAURANT_LIST } from "./utils/mockData";
 
 
-const Title = () => (
-        <h1 className="title">  
-        Sivagnanasambandhar Vazhga
-        </h1>
+function Header() {
+    let logo = new URL("./images/appLogo.jpg", import.meta.url);
+    return (
+        <div className="header-container">
+            <div className="logo-container">
+                <img src={logo} className="logo" />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
     );
+}
 
-const data = 10000;
+const RestaurantCard = (props) => {
+    const { resData } = props;
+    const {
+        cloudinaryImageId,
+        name,
+        cuisines,
+        avgRating,
+        costForTwo,
+        sla : {deliveryTime}
+    } = resData?.info;
 
-let HeadingComponent = () => (
-    <div id="container">
-         <Title></Title>
-         <Title/>
-         {Title()}
-        <h1 id="heading" className="headClass">Namaste React functional component </h1>
-    </div>
-);
+    return (
+        <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+            <img
+                className="res-logo"
+                alt="res-logo"
+                src={CDN_URL + cloudinaryImageId}
+            />
+            <h3>{name}</h3>
+            <h4>{cuisines?.join(",")}</h4>
+            <h4>{avgRating} stars</h4>
+            <h4>{costForTwo}</h4>
+            <h4>{deliveryTime} minutes</h4>
+        </div>
+    );
+}
+
+const Body = function () {
+    const restaurants = RESTAURANT_LIST?.data?.cards[5]?.card.card.gridElements?.infoWithStyle?.restaurants;
+    return (
+        <div className="body-container">
+            <div className="search">Search</div>
+            <div className="res-container">
+                {
+                //    restaurants.map((restaurant, index) => (
+                //      <RestaurantCard key = {index} resData = {restaurant} />
+                //    )) 
+
+                // Not recommended using index as keys -> Better to use unique ids (Better practice)
+                   restaurants.map((restaurant)=>(
+                    <RestaurantCard key={restaurant.info.id} resData={restaurant}/>
+                   ))
+                }
+            </div>
+        </div>
+    );
+}
 
 
+const AppLayout = () => {
+    return (
+        <div className="foodApp">
+            <Header />
+            <Body />
+        </div>
+    );
+}
 
 
 let root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
+
